87eab6dee1039f835d5efbf0598e446f22182c0b
Vadim Makhtarov <endervad5@gmail.com>
2023-05-23 01:59:24 +0300

    Add debug script for classes

---
+++ _Test_EXD.ps1
@@ -0,0 +1,16 @@
+# Sample script for debugging EXHF & EXDF classes
+
+using module .\lib\EXHF.psm1
+using module .\lib\EXDF.psm1
+
+$exh_path = '.\current\exh_source\exd\item.exh'
+
+$item_exh = [EXHF]::new( $exh_path )
+
+$item_0_exd = [EXDF]::new(
+	$item_exh.GetPage(0),
+	$item_exh.GetLang('en'),
+	'.\current\exd_source\exd\item_0_en.exd'
+)
+
+Pause
